73ee1dcaa5fcd45fe1ccc23d99b003d391157ffe
James Weng <wengj1@bxscience.edu>
2017-09-17 22:55:20 -0400

    Add beginnings of conversion

--- app/src/main/java/org/eu/weng/binhexcalc/MainActivity.java
+++ app/src/main/java/org/eu/weng/binhexcalc/MainActivity.java
@@ -11,12 +11,26 @@ public class MainActivity extends AppCompatActivity {
     private EditText numIn;
     private RadioGroup choice;
 
+    private String decToBin(String dec) {
+        return "";
+    }
+    private String decToHex(String dec) {
+        return "";
+    }
+    private String binToDec(String bin) {
+        return "";
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Sets the TextViews, EditText, and RadioGroups to the objects
-
+        decOut = (TextView)findViewById(R.id.decOut);
+        binOut = (TextView)findViewById(R.id.binOut);
+        hexOut = (TextView)findViewById(R.id.hexOut);
+        numIn = (EditText)findViewById(R.id.numIn);
+        choice = (RadioGroup)findViewById(R.id.choice);
     }
 }
