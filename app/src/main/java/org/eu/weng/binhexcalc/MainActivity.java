becf448fcd40dccc17710ca276a7917bde85db9e
James Weng <wengj1@bxscience.edu>
2017-09-17 20:50:33 -0400

    Add more buttons and begin coding

--- app/src/main/java/org/eu/weng/binhexcalc/MainActivity.java
+++ app/src/main/java/org/eu/weng/binhexcalc/MainActivity.java
@@ -1,13 +1,22 @@
 package org.eu.weng.binhexcalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView decOut, hexOut, binOut;
+    private EditText numIn;
+    private RadioGroup choice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Sets the TextViews, EditText, and RadioGroups to the objects
+
     }
 }
